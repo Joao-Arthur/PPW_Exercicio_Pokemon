de64e2eb03448675821e94a2fd3cdb4e4ca19ff4
Joao-Arthur <joao.lothamer@gmail.com>
2021-03-28 17:48:58 -0300

    ajuste showcase primeira exibição

--- showcase.js
+++ showcase.js
@@ -7,25 +7,32 @@ function onShowCaseClick() {
     const nomePokemon = document.getElementById('nomePokemon');
     const imagemPokemon = document.getElementById('imagemPokemon');
 
-    iterator = pokedex.values();
-
     if (intervalID) {
         window.clearInterval(intervalID);
         intervalID = 0;
         nomePokemon.classList.add('hidden');
         imagemPokemon.classList.add('hidden');
+        nomePokemon.textContent = '';
+        imagemPokemon.src = '';
     } else {
         nomePokemon.classList.remove('hidden');
         imagemPokemon.classList.remove('hidden');
-        intervalID = window.setInterval(() => {
-            const pokemon = getProximoPokemon();
-            nomePokemon.textContent = pokemon.nome;
-            imagemPokemon.src = pokemon.url;
-        }, TEMPO_EXIBICAO);
+        proximaImagemShowCase();
+        intervalID = window.setInterval(proximaImagemShowCase, TEMPO_EXIBICAO);
     }
 }
 
+function proximaImagemShowCase() {
+    const nomePokemon = document.getElementById('nomePokemon');
+    const imagemPokemon = document.getElementById('imagemPokemon');
+    const pokemon = getProximoPokemon();
+    nomePokemon.textContent = pokemon.nome;
+    imagemPokemon.src = pokemon.url;
+}
+
 function getProximoPokemon() {
+    if (!iterator) iterator = pokedex.values();
+
     let proximo = iterator.next();
     if (proximo.done) {
         iterator = pokedex.values();
